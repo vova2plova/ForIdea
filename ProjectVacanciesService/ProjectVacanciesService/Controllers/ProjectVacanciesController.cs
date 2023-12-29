5cc774a926c217681a395c4547ac33c5012c1dc9
Vladimir Iakovlev <wwevovancik@gmail.com>
2023-12-29 14:21:09 +0300

    Add ProjectVacanciesService

---
+++ ProjectVacanciesService/ProjectVacanciesService/Controllers/ProjectVacanciesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectVacanciesService.Controllers
+{
+    [Route("[controller]")]
+    public class ProjectVacanciesController : Controller
+    {
+        
+    }
+}
