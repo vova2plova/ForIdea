e06eb1996d2b0150cbe327493c9f6a9fe3dd39bf
Vladimir Iakovlev <wwevovancik@gmail.com>
2023-12-29 14:39:45 +0300

    Add WorkExperience service

---
+++ WorkExperienceService/WorkExperienceService/Controllers/WorkExperienceController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkExperienceService.Controllers
+{
+    [Route("[controller]")]
+    public class WorkExperienceController : Controller
+    {
+    }
+}
